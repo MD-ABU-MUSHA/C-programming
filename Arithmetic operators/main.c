ee492c0a48d048c7f863344c14f5a7e161deb3d3
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-24 02:00:48 +0600

    Add files via upload

---
+++ Arithmetic operators/main.c
@@ -0,0 +1,62 @@
+ #include<stdio.h>
+ #include<conio.h>
+ main()
+ {
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o;
+      printf("Enter The First Addition Number=");
+       scanf("%d",&a);
+          printf("\nEnter The Second Addition Number=");
+            scanf("%d",&b);
+                c=a+b;
+                   printf("\n------------------------------------");
+                    printf("\n            Addition Two Number=%d",c);
+                     printf("\n\n\n\n\nNow Subtraction\n\n");
+                       printf("Enter The First Subtraction Number=");
+                           scanf("%d",&d);
+                              printf("\nEnter The Second Subtraction Number=");
+                                scanf("%d",&e);
+                                    f=d-e;
+                                       printf("\n------------------------------------");
+                                        printf("\n           Subtraction Two Number=%d",f);
+
+
+                                           printf("\n\n\n\n\nNow Multiplication\n\n");
+                       printf("Enter The First Multiplication Number=");
+                           scanf("%d",&g);
+                              printf("\nEnter The Second Multiplication Number=");
+                                scanf("%d",&h);
+                                    i=g*h;
+                                       printf("\n-----------------------------------------");
+                                        printf("\n           Multiplication Two Number=%d",i);
+
+
+
+                                        printf("\n\n\n\n\nNow Division\n\n");
+                       printf("Enter The First Division Number=");
+                           scanf("%d",&j);
+                              printf("\nEnter The Second division Number=");
+                                scanf("%d",&k);
+                                    l=j/k;
+                                       printf("\n-----------------------------------");
+                                        printf("\n             Division Two Number=%d",l);
+
+
+
+
+                                        printf("\n\n\n\n\nNow Modulo Division\n\n");
+                       printf("Enter The First Modulo Division Number=");
+                           scanf("%d",&m);
+                              printf("\nEnter The Second Modulo Division Number=");
+                                scanf("%d",&n);
+                                    o=m%n;
+                                       printf("\n------------------------------------");
+                                        printf("\n           Modulo Division Two Number=%d",o);
+
+                                            getch();
+
+
+
+
+
+
+ }
