e32cc7a3e87ccb93e2b38e9888e3fe536f9c201c
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:26:32 +0600

    Add files via upload

---
+++ ODD or Even Number/main.cpp
@@ -0,0 +1,14 @@
+  #include<stdio.h>
+  #include<conio.h>
+  main()
+  {
+      int n;
+      scanf("%d",&n);
+      if(n%2==0)
+      {
+          printf("This Number is Even");
+      }else{
+        printf("This Number is ODD");
+           }getch();
+
+  }
