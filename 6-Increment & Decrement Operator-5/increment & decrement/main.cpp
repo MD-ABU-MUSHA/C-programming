c4f08f10d9c8c1f9c4aa62470e85b9f1ec243955
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:43:26 +0600

    Add files via upload

---
+++ 6-Increment & Decrement Operator-5/increment & decrement/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    int a=10, b=6,c;
+    c=(++a) - (b--);
+    printf("Result=%d",c);
+     printf("\n\nB=%d",b);
+     getch();
+}
---
+++ 6-Increment & Decrement Operator-5/increment/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a=10, b=6,c;
+    c=(++a) - (--b);
+    printf("Result=%d",c);
+    getch();
+}
