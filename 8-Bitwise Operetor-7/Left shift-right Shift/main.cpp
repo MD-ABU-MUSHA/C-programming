9e0f24fb936c02e63300bd04b2abf838312644cf
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:52:40 +0600

    Add files via upload

---
+++ 8-Bitwise Operetor-7/Left shift-right Shift/main.cpp
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a,b,c,d;
+    printf("Enter First Number=");
+        scanf("%d",&a);
+            printf("Enter Second Number=");
+                scanf("%d",&b);
+                    c=a<<2;
+                        printf("\nResult=%d",c);
+                            d=b>>2;
+                                printf("\n\nResult=%d",d);
+                                    getch();
+}
