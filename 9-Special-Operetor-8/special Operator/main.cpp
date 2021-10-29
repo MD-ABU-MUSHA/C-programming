ba9fad9b7ad2a582a8ba92bf77267778730bba98
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:53:26 +0600

    Add files via upload

---
+++ 9-Special-Operetor-8/special Operator/main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a,b,c;
+        printf("Enter First Number=");
+            scanf("%d",&a);
+        printf("Enter Second Number=");
+            scanf("%d",&b);
+    c=(a,b,a+b);
+    printf("Result=%d",c);
+    getch();
+}
