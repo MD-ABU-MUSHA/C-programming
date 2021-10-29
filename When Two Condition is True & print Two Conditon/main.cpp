ea041511f2d4dd6e53350776524c3400e4c493d0
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:27:40 +0600

    Add files via upload

---
+++ When Two Condition is True & print Two Conditon/main.cpp
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ #include<conio.h>
+ main()
+ {
+     int n;
+     scanf("%d",&n);
+     if(n<50)
+     {
+         printf("n lessthen 50");
+     }
+     if(n<80)
+     {
+         printf("\n\nn lessthen 80");
+     }getch();
+ }
