9dbda0c1c4d17ef9e31d96cbe927876b4d0152db
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:27:58 +0600

    Add files via upload

---
+++ When Two condition is True But print First/main.cpp
@@ -0,0 +1,15 @@
+  #include <stdio.h>
+  #include<conio.h>
+ int main()
+ {
+     int n ;
+     scanf("%d",&n);
+     if (n < 30) {
+         printf("n is less than 30.\n");
+     }
+     else if(n < 50) {
+         printf("n is less than 50.\n");
+     }
+
+     getch();
+ }
