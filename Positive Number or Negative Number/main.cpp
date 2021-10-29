3a1daeb6ebda669f6d69f68941e4d92225421e43
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:27:18 +0600

    Add files via upload

---
+++ Positive Number or Negative Number/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int n;
+    scanf("%d",&n);
+    if( n>=0 )
+    {
+        printf("This Number is  Positive");
+    }else{
+        printf("This Number is Negative");
+        }getch();
+
+}
