dfc569ded9c190900beefabbb787aa0f23a9aa23
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:39:40 +0600

    Add files via upload

---
+++ 5-Assignment Operator-4/main.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ #include<conio.h>
+main()
+{
+    int i;
+     printf("Enter Any Number=");
+        scanf("%d",&i);
+            i*=100;
+                printf("Result=%d",i);
+                    getch();
+}
