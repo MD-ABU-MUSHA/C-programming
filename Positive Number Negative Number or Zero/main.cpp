eec88273c96230460a2e1be2c805b67b5f5dd414
MD ABU MUSHA <59597742+MD-ABU-MUSHA@users.noreply.github.com>
2021-10-29 22:26:53 +0600

    Add files via upload

---
+++ Positive Number Negative Number or Zero/main.cpp
@@ -0,0 +1,30 @@
+ /*#include<stdio.h>
+ #include<conio.h>
+ main()
+ {
+     int n;
+     scanf("%d",&n);
+     if( n<0)
+     {
+         printf("This Number is Negative");
+     }else if( n>0)
+     {
+         printf("This Number is Positive");
+     }else if(n==0)
+     {
+         printf("This Number is Zero");
+     }getch();
+ }*/
+
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int n;
+    scanf("%d",&n);
+        if(n<0){printf("This Number is Negative");}
+    else if(n>0){printf("This Number is Positive");}
+        else{printf("This Number is Zero");}
+    getch();
+}
